cd68b70bcf75fecaa7ce8690452c2d0fdfb31880
Samantha <samantha.c.alvarenga@gmail.com>
2024-09-20 03:30:48 -0300

    Exercicio 2 - C# Criando uma classe chamada ContaPoupança, e construtor padrão

---
+++ Ativ-ContaBancaria/models/ContaPoupanca.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ativ_ContaBancaria.models
+{
+    internal class ContaPoupanca
+    {
+        public string Titular { get; set; }
+        public string NumeroConta { get; set; }
+        public double Saldo { get; set; }
+        public DateTime DataAniversario { get; set; }
+    }
+}
